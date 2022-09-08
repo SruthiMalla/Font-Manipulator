2a8356d9c143c9ef631d339511d518f35f867069
SruthiMalla <52434141+SruthiMalla@users.noreply.github.com>
2022-09-08 16:57:12 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,10 +26,25 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+
+        difference = floor(leftwristX - rightwristX);
+        console.log("leftwristX = " + leftwristX + "rightwristX = " + rightwristX + "difference = " + difference);
     }
 }
 
 function draw()
 {
     background("#969A97");
+
+    document.getElementById("square_side").innerHTML = "The font size of the text = " + difference;
+    fill("blue");
+    textSize(difference);
+    text("Sahas", noseX, noseY);
 }
